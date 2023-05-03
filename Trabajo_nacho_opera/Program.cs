bdfec563d49ddb8937a38d8c8af1a4848ef59df3
juliana <WINDOSW 10@DESKTOP-KQUVA2I>
2023-05-03 17:21:57 -0500

    Agregar archivos de proyecto.

---
+++ Trabajo_nacho_opera/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nachoOpera;
+
+namespace Trabajo_nacho_opera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingrese el primer numero ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Ingrese el segundo numero ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double suma = operaciones.sumar(num1, num2);
+            double resta = operaciones.restar(num1, num2);
+            double mult = operaciones.multiplicacion(num1, num2);
+            double div = operaciones.division(num1, num2);
+
+            Console.WriteLine(" El resultado de la suma es: " + suma);
+            Console.WriteLine(" El resultado de la resta es: " + resta);
+            Console.WriteLine(" El resultado de la multiplicacion es: " + mult);
+            if(num2 == 0)
+            {
+                Console.WriteLine(" Division por 0 no existe");
+            }
+            else
+            {
+                Console.WriteLine(" El resultado de la division es: " + div);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ nachoOpera/operaciones.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nachoOpera
+{
+    public class operaciones
+    {
+        /// <summary>
+        /// metodo sumar
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static double sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        /// <summary>
+        /// metodo restar
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static double restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        /// <summary>
+        /// metodo multipicar
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static double multiplicacion(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        /// <summary>
+        /// metodo dividir 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static double division(double num1, double num2)
+        {
+            if(num2 == 0)
+            {
+                Console.WriteLine(" Division por 0 no existe");
+            }
+                return num1 / num2;
+            
+            
+        }
+    }
+}
